dca932a50c86649ae387018d1002bb3601b6ec99
hdelios <cpt.meo.vmn@gmail.com>
2025-06-15 17:28:36 +0700

    use geth params

--- precompiles/common/params.go
+++ precompiles/common/params.go
@@ -1,7 +1,11 @@
 package common
 
+import (
+	"github.com/ethereum/go-ethereum/params"
+)
+
 const (
 	Ed25519VerifyGas uint64 = 1500
-	Sha512BaseGas    uint64 = 60 // Base price for a SHA512 operation
-	Sha512PerWordGas uint64 = 12 // Per-word price for a SHA512 operation
+	Sha512BaseGas    uint64 = params.Sha256BaseGas
+	Sha512PerWordGas uint64 = params.Sha256PerWordGas
 )
